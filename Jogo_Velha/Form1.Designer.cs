d6ec3b99af8660ebd31d40cf5e73062722e6ff0c
Josi <josidarcs@gmail.com>
2013-09-17 10:51:20 -0300

    Primeiro Envio

---
+++ Jogo_Velha/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Jogo_Velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.ForeColor = System.Drawing.Color.Green;
+            this.b11.Location = new System.Drawing.Point(6, 11);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(100, 100);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = true;
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.ForeColor = System.Drawing.Color.Green;
+            this.b12.Location = new System.Drawing.Point(112, 11);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(100, 100);
+            this.b12.TabIndex = 1;
+            this.b12.UseVisualStyleBackColor = true;
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.ForeColor = System.Drawing.Color.Green;
+            this.b13.Location = new System.Drawing.Point(218, 11);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(100, 100);
+            this.b13.TabIndex = 2;
+            this.b13.UseVisualStyleBackColor = true;
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.ForeColor = System.Drawing.Color.Green;
+            this.b23.Location = new System.Drawing.Point(218, 117);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(100, 100);
+            this.b23.TabIndex = 5;
+            this.b23.UseVisualStyleBackColor = true;
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.ForeColor = System.Drawing.Color.Green;
+            this.b22.Location = new System.Drawing.Point(112, 117);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(100, 100);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.ForeColor = System.Drawing.Color.Green;
+            this.b21.Location = new System.Drawing.Point(6, 117);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(100, 100);
+            this.b21.TabIndex = 3;
+            this.b21.UseVisualStyleBackColor = true;
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.ForeColor = System.Drawing.Color.Green;
+            this.b33.Location = new System.Drawing.Point(218, 223);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(100, 100);
+            this.b33.TabIndex = 8;
+            this.b33.UseVisualStyleBackColor = true;
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.ForeColor = System.Drawing.Color.Green;
+            this.b32.Location = new System.Drawing.Point(112, 223);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(100, 100);
+            this.b32.TabIndex = 7;
+            this.b32.UseVisualStyleBackColor = true;
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.ForeColor = System.Drawing.Color.Green;
+            this.b31.Location = new System.Drawing.Point(6, 223);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(100, 100);
+            this.b31.TabIndex = 6;
+            this.b31.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Começa o jogador X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(351, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "O";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 302);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(393, 328);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(393, 354);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(59, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(328, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Empates";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(481, 389);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Jogo_Velha/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Jogo_Velha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool xis = true;
+        int ganhador1=0;
+        int ganhador2=0;
+        int emapates = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            b11.Click += new EventHandler(BClick);
+            b12.Click += new EventHandler(BClick);
+            b13.Click += new EventHandler(BClick);
+            b21.Click += new EventHandler(BClick);
+            b22.Click += new EventHandler(BClick);
+            b23.Click += new EventHandler(BClick);
+            b31.Click += new EventHandler(BClick);
+            b32.Click += new EventHandler(BClick);
+            b33.Click += new EventHandler(BClick);
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.TabStop = false;
+                }
+            }
+        }
+
+        private void BClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.xis ? "X" : "O";
+            ((Button)sender).Enabled = false;
+            VerificarGanhador();
+            xis = !xis;
+            label1.Text = string.Format("{0}, É a sua vez", this.xis ? "X" : "O");
+        }
+
+        private void VerificarGanhador()
+        {
+            if (
+                    b11.Text != String.Empty && b11.Text == b12.Text && b12.Text == b13.Text ||
+                    b21.Text != String.Empty && b21.Text == b22.Text && b22.Text == b23.Text ||
+                    b31.Text != String.Empty && b31.Text == b32.Text && b32.Text == b33.Text ||
+
+                    b11.Text != String.Empty && b11.Text == b21.Text && b21.Text == b31.Text ||
+                    b12.Text != String.Empty && b12.Text == b22.Text && b22.Text == b32.Text ||
+                    b13.Text != String.Empty && b13.Text == b23.Text && b23.Text == b33.Text ||
+
+                    b11.Text != String.Empty && b11.Text == b22.Text && b22.Text == b33.Text ||
+                    b13.Text != String.Empty && b13.Text == b22.Text && b22.Text == b31.Text
+                )
+            {
+                if (xis == true)
+                {
+                    textBox1.Text = ganhador1.ToString() + 1;
+
+                }
+                else
+                {
+                    textBox2.Text = ganhador2.ToString() + 1;
+                }
+                MessageBox.Show(String.Format("O ganhador é o [{0}]", xis ? "X" : "O"), "Temos um vencedor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reiniciar();
+
+            }
+            else
+            {
+                VerificarEmpate();
+            }
+        }
+
+        private void VerificarEmpate()
+        {
+            bool todosDesabilitados = true;
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button && item.Enabled)
+                {
+                    todosDesabilitados = false;
+                    break;
+                }
+            }
+            if (todosDesabilitados)
+            {
+                textBox3.Text = (emapates+=1).ToString();
+                MessageBox.Show(String.Format("Deu empate"), "Ops!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Reiniciar();
+            }
+        }
+
+        private void Reiniciar()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = true;
+                    item.Text = String.Empty;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
